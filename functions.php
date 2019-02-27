e89dd24750e9bf8f425448e45747435a7addf94b
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-02-26 18:31:31 -0800

    Initial commit

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once(dirname(__FILE__).'/vendor/autoload.php');
+
+// Mustache
+$ms_t = new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/_includes'),
+)); // '_includes/' engine
+
+$ms = new Mustache_Engine;
+
+// Config
+$config = json_decode(file_get_contents(dirname(__FILE__).'/_config.json'));
+
+// Database
+use Medoo\Medoo;
+$db_config = json_decode(file_get_contents(dirname(__FILE__).'/_db-config.json'));
+
+$db = new Medoo([
+    'database_type' => $db_config->{'database_type'},
+    'database_name' => $db_config->{'database_name'},
+    'server' => $db_config->{'server'},
+    'username' => $db_config->{'username'},
+    'password' => $db_config->{'password'}
+]);
+
+// Parsedown
+$Parsedown = new Parsedown();
+
+?>
---
+++ head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="/_assets/github-markdown.css">
+    <link rel="stylesheet" href="/_assets/bundle.css">
+</head>
+<body>
