ce28ed7363ca6a114f39fc5b0c45be3d130785ba
Jack Greenberg <j.lester.greenberg@gmail.com>
2019-02-27 12:36:58 -0800

    Comment out config code

--- functions.php
+++ functions.php
@@ -9,20 +9,20 @@ $ms_t = new Mustache_Engine(array(
 
 $ms = new Mustache_Engine;
 
-// Config
-$config = json_decode(file_get_contents(dirname(__FILE__).'/_config.json'));
+// // Config
+// $config = json_decode(file_get_contents(dirname(__FILE__).'/_config.json'));
 
-// Database
-use Medoo\Medoo;
-$db_config = json_decode(file_get_contents(dirname(__FILE__).'/_db-config.json'));
-
-$db = new Medoo([
-    'database_type' => $db_config->{'database_type'},
-    'database_name' => $db_config->{'database_name'},
-    'server' => $db_config->{'server'},
-    'username' => $db_config->{'username'},
-    'password' => $db_config->{'password'}
-]);
+// // Database
+// use Medoo\Medoo;
+// $db_config = json_decode(file_get_contents(dirname(__FILE__).'/_db-config.json'));
+//
+// $db = new Medoo([
+//     'database_type' => $db_config->{'database_type'},
+//     'database_name' => $db_config->{'database_name'},
+//     'server' => $db_config->{'server'},
+//     'username' => $db_config->{'username'},
+//     'password' => $db_config->{'password'}
+// ]);
 
 // Parsedown
 $Parsedown = new Parsedown();
